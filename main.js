6f862aa4ec04aa17d8d96898d4d6ac88e89c0dd0
alannasheridan <114628210+alannasheridan@users.noreply.github.com>
2025-03-26 23:39:53 -0400

    replace

---
+++ main.js
@@ -0,0 +1,25 @@
+// const navbar = document.querySelector('.navbar');
+// const scrollWatcher = document.createElement('div');
+
+// scrollWatcher.setAttribute('data-scroll-watcher', '');
+// navbar.before(scrollWatcher);
+
+// const navObserver = new IntersectionObserver((entries) => {
+//     console.log(entries)
+//     navbar.classList.toggle('sticking')
+// });
+
+// navObserver.observe(scrollWatcher)
+
+// window.onscroll = function() {
+//     document.getElementById("topBtn").style.display = window.scrollY > 200 ? "block" : "none";
+// };
+
+// function topFunction() {
+//     window.scrollTo({ top: 0, behavior: "smooth" });
+// }
+
+// {/* <script> document.getElementById('currentYear').textContent = new Date().getFullYear(); </script> */}
+
+
+//-------- videos --------//
